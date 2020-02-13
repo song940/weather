bac08e61d91f4b6b14c925d0e9cc014f3d97b5c9
Liu Song <song940@gmail.com>
2020-02-13 15:32:43 +0800

    Initial commit

---
+++ src/pages/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />,
+  document.getElementById('app'));
